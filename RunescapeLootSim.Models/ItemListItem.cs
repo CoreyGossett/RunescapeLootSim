ea213ce4450939ef798262c23ecef472049220a1
Corey Gossett <coreymgossett@gmail.com>
2022-04-05 18:15:35 -0400

    Added ItemController, Model, and basic View

---
+++ RunescapeLootSim.Models/ItemListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunescapeLootSim.Models
+{
+    public class ItemListItem
+    {
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+        public int Damage { get; set; }
+    }
+}
---
+++ RunescapeLootSim.WebMVC/Controllers/ItemController.cs
@@ -0,0 +1,20 @@
+﻿using RunescapeLootSim.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RunescapeLootSim.WebMVC.Controllers
+{
+    [Authorize]
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Index()
+        {
+            var model = new ItemListItem[0];
+            return View(model);
+        }
+    }
+}
